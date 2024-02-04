66b2fcbf11d8fccaecae7eaec7aae06c6ed39862
jeanmarie samakila <samakilambilikajeanmarie@gamil.com>
2024-02-04 17:31:20 +0200

    installation sur serveur et creation de l'API

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.listen()
+// app.use(express.json());
+
+app.get('/', (req, res) => {
+    const tweets = [
+        {id: 1, text: 'first tweets'},
+        {id: 2, text: 'second tweets'},
+        {id: 3, text: 'three tweets'},
+        {id: 4, text: 'four tweets'}
+    ];
+    res.json(tweets);
+    // res.send('bienvenu sur notre page')
+});
+
+app.post('/tweets', (req, res) => {
+    const newTweet = req.body;
+    res.json({message: 'Tweet posté avec succès', tweet: newTweet});
+});
+
+
+
+app.listen(port, () => {
+    console.log("le serveur est operationel");
+} )
