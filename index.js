e643a9d5002d731031adbf1b9d8fb1bcb532b401
jeanmarie samakila <samakilambilikajeanmarie@gmail.com>
2024-02-06 11:51:18 +0200

    creation du json et mise en place de get et post

--- index.js
+++ index.js
@@ -1,24 +1,80 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3002;
 
 // app.use(express.json());
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
     const tweets = [
-        {id: 1, text: 'first tweets'},
-        {id: 2, text: 'second tweets'},
-        {id: 3, text: 'three tweets'},
-        {id: 4, text: 'four tweets'}
-    ];
-    res.json(tweets);
-    // res.send('bienvenu sur notre page')
-});
+        {
+            id: 1,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+            url: "https://via.placeholder.com/600/92c952",
+            thumbnailUrl: "https://via.placeholder.com/150/92c952",
+            like: 9122,
+            repost: 10
+            
+        },
+        {
+            id: 2,
+            title: "qui est esse",
+            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+            url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
+            url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
+            thumbnailUrl: "",
+            like: 122,
+            repost: 7
+        },
+        {
+            id: 3,
+            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+            url: "https://via.placeholder.com/600/92c952",
+            thumbnailUrl: "https://via.placeholder.com/150/92c952",
+            like: 9122,
+            repost: 10
 
-app.post('/tweets', (req, res) => {
-    const newTweet = req.body;
-    res.json({message: 'Tweet posté avec succès', tweet: newTweet});
-});
+        },
+        {
+            id: 4,
+            title: "qui est esse",
+            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+            url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
+            thumbnailUrl: "https://via.placeholder.com/150/92c952",
+            like: 122,
+            repost: 7
+        }
+    ];
+//     res.json(tweets);
+// });
+
+app.get("/", (req, res,) => {
+    res.send(tweets)
+})
+
+app.get('/:id', (req, res) => {
+    const id = parseInt (req.params.id)
+    res.send(tweets.filter((e) => {
+        return e.id === id
+    }))
+})
+
+
+app.delete("/:id", (req, res) => {
+    const id = parseInt (req,params.id)
+    res.send(tweets.filter((e) => {
+        let element = e.id === id
+        return tweets.splice(id-1,1)
+    }))
+})
+
+app.use(express.json());
+
+app.post("/post", (req, res) => {
+    tweets.push(req, body)
+    res.send('le poste a ete creer')
+} )
 
 
 
