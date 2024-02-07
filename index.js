c962a5544b4cfac8b4029f5b63cce502bb945e51
jeanmarie samakila <samakilambilikajeanmarie@gmail.com>
2024-02-07 20:14:25 +0200

    finish first part

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3002;
+const port = 3000;
 
 // app.use(express.json());
 
@@ -11,7 +11,6 @@ const port = 3002;
             title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
             body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
             url: "https://via.placeholder.com/600/92c952",
-            thumbnailUrl: "https://via.placeholder.com/150/92c952",
             like: 9122,
             repost: 10
             
@@ -21,8 +20,6 @@ const port = 3002;
             title: "qui est esse",
             body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
             url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
-            url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
-            thumbnailUrl: "",
             like: 122,
             repost: 7
         },
@@ -31,7 +28,6 @@ const port = 3002;
             title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
             body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
             url: "https://via.placeholder.com/600/92c952",
-            thumbnailUrl: "https://via.placeholder.com/150/92c952",
             like: 9122,
             repost: 10
 
@@ -50,34 +46,39 @@ const port = 3002;
 // });
 
 app.get("/", (req, res,) => {
-    res.send(tweets)
-})
+    res.send(tweets);
+});
 
 app.get('/:id', (req, res) => {
     const id = parseInt (req.params.id)
+    const tweet = tweets.find(tweet => tweet.id === id);
     res.send(tweets.filter((e) => {
         return e.id === id
     }))
-})
+});
 
 
 app.delete("/:id", (req, res) => {
-    const id = parseInt (req,params.id)
-    res.send(tweets.filter((e) => {
-        let element = e.id === id
-        return tweets.splice(id-1,1)
-    }))
-})
+    const id = parseInt(req.params.id);
+    const index = tweets.findIndex(tweet => tweet.id === id);
+    tweets.splice(index, 1);
+    res.send("Tweet supprimé");
 
-app.use(express.json());
+});
 
+app.use(express.json())
 app.post("/post", (req, res) => {
-    tweets.push(req, body)
-    res.send('le poste a ete creer')
-} )
+    tweets.push(req.body);
+    res.send('Le poste a été créé');
+});
 
-
+app.put("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = tweets.findIndex(tweet => tweet.id === id);
+    tweets[index] = req.body;
+    res.send("Tweet mis à jour avec succès");
+});
 
 app.listen(port, () => {
     console.log("le serveur est operationel");
-} )
+})
