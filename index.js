097d0563548bf96c6b4253d425551f8fee22d48e
jeanmarie samakila <samakilambilikajeanmarie@gmail.com>
2024-02-08 17:39:24 +0200

    mise en place des routes

--- index.js
+++ index.js
@@ -1,84 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-// app.use(express.json());
-
-// app.get('/', (req, res) => {
-    const tweets = [
-        {
-            id: 1,
-            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            url: "https://via.placeholder.com/600/92c952",
-            like: 9122,
-            repost: 10
-            
-        },
-        {
-            id: 2,
-            title: "qui est esse",
-            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-            url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
-            like: 122,
-            repost: 7
-        },
-        {
-            id: 3,
-            title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
-            url: "https://via.placeholder.com/600/92c952",
-            like: 9122,
-            repost: 10
-
-        },
-        {
-            id: 4,
-            title: "qui est esse",
-            body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-            url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
-            thumbnailUrl: "https://via.placeholder.com/150/92c952",
-            like: 122,
-            repost: 7
-        }
-    ];
-//     res.json(tweets);
-// });
-
-app.get("/", (req, res,) => {
-    res.send(tweets);
-});
-
-app.get('/:id', (req, res) => {
-    const id = parseInt (req.params.id)
-    const tweet = tweets.find(tweet => tweet.id === id);
-    res.send(tweets.filter((e) => {
-        return e.id === id
-    }))
-});
+const routes = require('./routes');
 
 
-app.delete("/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = tweets.findIndex(tweet => tweet.id === id);
-    tweets.splice(index, 1);
-    res.send("Tweet supprimé");
-
-});
-
-app.use(express.json())
-app.post("/post", (req, res) => {
-    tweets.push(req.body);
-    res.send('Le poste a été créé');
-});
-
-app.put("/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = tweets.findIndex(tweet => tweet.id === id);
-    tweets[index] = req.body;
-    res.send("Tweet mis à jour avec succès");
-});
+app.use(express.json());
+app.use('/', routes);
 
 app.listen(port, () => {
     console.log("le serveur est operationel");
-})
+});
---
+++ routes.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+
+const tweets = [
+                {
+                    id: 1,
+                    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+                    url: "https://via.placeholder.com/600/92c952",
+                    like: 9122,
+                    repost: 2
+                    
+                },
+                {
+                    id: 2,
+                    title: "qui est esse",
+                    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+                    url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
+                    like: 122,
+                    repost: 7
+                },
+                {
+                    id: 3,
+                    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+                    url: "https://via.placeholder.com/600/92c952",
+                    like: 9122,
+                    repost: 10
+        
+                },
+                {
+                    id: 4,
+                    title: "qui est esse",
+                    body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+                    url: "https://www.pexels.com/photo/woman-in-white-long-sleeved-top-and-skirt-standing-on-field-2880507/",
+                    thumbnailUrl: "https://via.placeholder.com/150/92c952",
+                    like: 122,
+                    repost: 7
+                }
+            ];
+            
+router.get("/", (req, res) => {
+    res.send(tweets);
+});
+
+router.get('/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const tweet = tweets.find(tweet => tweet.id === id);
+    res.json(tweet);
+});
+
+router.delete("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = tweets.findIndex(tweet => tweet.id === id);
+    tweets.splice(index, 1);
+    res.send("Tweet supprimé");
+});
+
+router.post("/", (req, res) => {
+    tweets.push(req.body);
+    res.send('Le tweet a été créé');
+});
+
+router.put("/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const index = tweets.findIndex(tweet => tweet.id === id);
+    tweets[index] = req.body;
+    res.send("Tweet mis à jour avec succès");
+});
+
+module.exports = router;
